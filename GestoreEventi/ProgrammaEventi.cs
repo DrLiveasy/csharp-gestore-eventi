e99e47c8078f676d99cacd37e1ad20d85dfa14f1
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-13 15:19:51 +0100

    creata classe ProgrammaEventi

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class ProgrammaEventi
+    {
+        #region Proprietà
+        private string Titolo;
+        private List<Evento> eventi;
+        #endregion
+    }
+}
