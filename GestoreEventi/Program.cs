b1481b3839bc515aff987d5cafe3e96a8e42b8e6
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-13 13:20:03 +0100

    istanziare un evetno dall'input utente

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GestoreEventi;
+
 Console.WriteLine("Hello, World!");
+
+Console.Write("Inserisci il nome dell'evento: ");
+string nomeEvento = Console.ReadLine();
+Console.Write("Inserisci data dell'evento (gg/mm/yyyy): ");
+string dataStringa = Console.ReadLine();
+Console.Write("Inserisci il numero di posti totali: ");
+int capienzaMax = int.Parse(Console.ReadLine());
+
+Evento EventoInput = new Evento(nomeEvento, dataStringa, capienzaMax);
+
