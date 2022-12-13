4a4afc0284926b5315c06234f3c677d75d8e2a67
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-13 12:28:17 +0100

    impostati getters e setters

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -13,8 +13,97 @@ namespace GestoreEventi
         private string Titolo;
         private string Data;
         //Stato
-        private string CapienzaMax;
-        private string PostiPrenotati;
+        private int CapienzaMax;
+        private int PostiPrenotati;
+        
         #endregion
+
+        #region Costruttori
+        public Evento(string titolo, string data, int capienzaMax, int postiPrenotati)
+        {
+            Titolo = titolo;
+            Data = data;
+            CapienzaMax = capienzaMax;
+            PostiPrenotati = postiPrenotati;
+        }
+
+        #endregion
+
+        #region Getters-Seters
+        public string GetTitolo1()
+        {
+            return Titolo;
+        }
+        public void SetTitolo1(string value)
+        {
+            if (value == "")
+            {
+                throw new Exception("Il Titolo non può essere vuoto!");
+            }
+            else
+            {
+                this.Titolo = value;
+            }
+        }
+        public string GetData1()
+        {
+            return Data;
+        }
+        public void SetData1(string value)
+        {
+            DateTime dataOraAttuale = DateTime.Now;
+            DateTime data = DateTime.Parse(value);
+            if (value == "")
+            {
+                throw new Exception("la data non può essere vuota!");
+            }
+            else if (dataOraAttuale > data)
+            {
+                throw new Exception("non puoi inserire una data passata!");
+            }
+            else
+            {
+                this.Data = value;
+            }
+        }
+        public int GetCapienzaMax1()
+        {
+            return CapienzaMax;
+        }
+
+        public void SetCapienzaMax1(int value)
+        {
+            if (value < 0 ) 
+            {
+                throw new Exception("la capienza massima non può essere un numero negativo !");
+            }
+            else
+            {
+                this.CapienzaMax = value;
+            }
+        }
+
+        public int GetPostiPrenotati1()
+        {
+            return PostiPrenotati;
+        }
+
+        
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
+
